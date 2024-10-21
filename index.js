d36801f2d6c84532a356a554ac54972dc3f12150
Lazy-dev-hash <nelzys52@gmail.com>
2024-10-21 21:26:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const { Configuration, OpenAIApi } = require('openai');
 
 // Replace these with your actual tokens
-const PAGE_ACCESS_TOKEN = 'EAAZAla0AZCCdUBO3OKJcabVB9XkemqZB6ApXbaTLtZAWDGo1rW3VtBcJFZAx9UXUZChBblR0AJSdZCuTJsm51NKOsuvT28d6J6XHI9kqqerdrHA66RW1kDjYDjuuPMYeURt7gQ7MZAp8woSgpfQJpPehOJWTbEWT26HwGB3aVElBFihx0B87AtTuAzCGZCTy4aZC41swZDZD';
+const PAGE_ACCESS_TOKEN = 'EAAZAla0AZCCdUBO3ZAF6EZAHoeHb2FI3tXq2PkwJhEiXpEGjbMUWBKYQnOrguZBGc2DsZBXhUD20KRqEnpvzpPYvnAWtxWbdu1jUMQ2o9rzZBh7PiNNnZAj3siIGas796XnKWn0OyfKLAauAZBjbwQmbcr4GlW1uE33JrcdKWOxMM3ZCSonwZBTWU91G1DNndgpTY9TBAZDZD';
 const VERIFY_TOKEN = 'pagebot';
 const OPENAI_API_KEY = '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';
 
